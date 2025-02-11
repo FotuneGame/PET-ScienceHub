a138dffb44d3b4e79789608152bc94d18c78e674
Grigory Titov <titovgrisha04@gmail.com>
2025-02-11 22:37:48 +0300

    feat: Add phone sms sender from MTS
    But we need to be legal entity
    
    task: #9

---
+++ user/src/phone/data.ts
@@ -0,0 +1,4 @@
+export const data = {
+    url: "https://api.exolve.ru/messaging/v1/SendSMS",
+    text: "Код подтверждения для freeai: "
+}
--- user/src/phone/index.ts
+++ user/src/phone/index.ts
@@ -1,10 +1,21 @@
 import HandlerError from "../error";
+import {data} from "./data";
+import axios from "axios";
+
+
 
 
 
 export async function sendSMS(code: number, phone: string, callback: ()=>void){
 
     try{
+        await axios.post(data.url,{
+          number: process.env.PHONE,
+          destination: phone,
+          text: data.text + code
+        },{
+          headers: {'Authorization': 'Bearer ' + process.env.PHONE_KEY}
+        })
         callback();
         return;
     }catch(err){
